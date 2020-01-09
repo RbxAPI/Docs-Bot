547a04115b8c048559c7e1b6c42bf39567964452
Sanjay <Sanjay-B@users.noreply.github.com>
2020-01-09 17:15:00 -0500

    [Bat] Implemented a way to run the bot
    For windows. Quick start file.

---
+++ run.bat
@@ -0,0 +1,7 @@
+REM Windows Quick-Start file
+REM Directions: Click & start. You can run this with or without administrative priviledges.
+@echo off
+chcp 65001
+
+REM Add check to see if Python Version is >= 3.7.x
+%SYSTEMROOT%\py.ex main.py
