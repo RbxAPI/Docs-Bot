59f3b6846a67b77d04140d37c33ae77fff48bf2a
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-01-10 14:53:01 -0500

    [Docs] OS System checks for Generate.py

--- generate.py
+++ generate.py
@@ -6,8 +6,15 @@ if __name__ == '__main__':
     #https://raw.githubusercontent.com/RbxAPI/Docs-Bot/rewrite/main.py
     response = requests.get("https://raw.githubusercontent.com/RbxAPI/Docs-Bot/rewrite/main.py")
     with open("main.py",'w+') as file:
-        file.write(response.text)
+        file.write(str((response.text).encode("utf-8")))
     file.close()
 
     # This is incredibly hacky but, eh.
-    os.system('python3 main.py')
+
+    # If Mac then, ...
+    if os.name == "posix":
+        os.system('python3 main.py')
+    
+    # If Windows then, ...
+    elif os.name == "nt":
+        os.system('python main.py')
