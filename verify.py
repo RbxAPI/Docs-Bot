743739788301cf659c824f7f15a140c5c54f370b
Pythonic-Rainbow <wongchingho0@gmail.com>
2020-01-10 20:38:16 +0000

    Improves code efficiency and update list command (#19)
    * Reformat and improves efficiency in current code
    
    * [Req] Lock Discord.py version to 1.2.5
    
    1.2.4 is broken and conflicts with reactions adaptation regarding ?subscribe
    
    * [Req] Unlock Discord.py version 1.2.5+
    
    * [Docs] Fixed title redundancy in ?list command header
    
    * [Docs] Implement Autogenerate based on git (generate.py), !restart command
    
    * [Docs] OS System checks for Generate.py
    
    * [Docs] Hotfix for internal encoding issues via Windows
    
    * [Docs] Comment Cleanup ?codeblocks
    
    * [Docs] Remove rogue command call
    
    * [Docs] Hotfix for encoding issues via Windows
    
    * Reformat and improves efficiency in current code
    
    Co-authored-by: Sanjay <Sanjay-B@users.noreply.github.com>

--- verify.py
+++ verify.py
@@ -1,10 +1,12 @@
 import discord, random, aiohttp
 from discord.ext import commands
 
+
 class Verify(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.words = ['apple', 'pie', 'cheese', 'dog', 'cat', 'sanjay', 'pancake', 'bloxy cola', 'bloxy is bad', 'node.js is bad', 'python is good.']
+        self.words = ['apple', 'pie', 'cheese', 'dog', 'cat', 'sanjay', 'pancake', 'bloxy cola', 'bloxy is bad',
+                      'node.js is bad', 'python is good.']
         self.client = aiohttp.ClientSession()
 
     async def UsernameById(self, user):
@@ -20,16 +22,18 @@ class Verify(commands.Cog):
         r = await r.json()
         if r['status'] == 'ok':
             username = await self.UsernameById(r['robloxId'])
-            await ctx.message.member.edit(nick=username)
-            await ctx.message.member.add_roles(336577687529193472)
+            await ctx.author.edit(nick=username)
+            await ctx.author.add_roles(336577687529193472)
             embed = discord.Embed(
                 title='User Verified',
-                description=f'<@{member.id}> has been verified as {username}',
+                description=f'{ctx.author.mention} has been verified as {username}',
                 color=0x008e00
             )
             await ctx.send(embed=embed)
         else:
-            await ctx.send(f'<@{ctx.message.author.id}> :exclamation::wave: You must be new! Please go to https://verify.eryn.io/ and follow the instructions on the page in order to get verified.')
+            await ctx.send(
+                f'{ctx.author.mention} :exclamation::wave: You must be new! Please go to '
+                f'https://verify.eryn.io/ and follow the instructions on the page in order to get verified.')
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -42,13 +46,14 @@ class Verify(commands.Cog):
             await member.add_roles(336577687529193472)
             embed = discord.Embed(
                 title='User Verified',
-                description=f'<@{member.id}> has been verified as {username}',
+                description=f'{member.mention} has been verified as {username}',
                 color=0x008e00
             )
             await member.guild.get_channel(335789594236813313).send(embed=embed)
         else:
             pass
-            #not in eryn db
+            # not in eryn db
+
 
 def setup(bot):
     bot.add_cog(Verify(bot))
