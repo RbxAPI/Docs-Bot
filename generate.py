a20106887ed356f97d2f89dc319d07f72269c4de
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-01-11 17:15:33 -0500

    [Gen] Fixed Encoding issues on Windows

--- generate.py
+++ generate.py
@@ -5,6 +5,29 @@ import os
 if __name__ == '__main__':
     #https://raw.githubusercontent.com/RbxAPI/Docs-Bot/rewrite/main.py
     response = requests.get("https://raw.githubusercontent.com/RbxAPI/Docs-Bot/rewrite/main.py")
-    with open("main.py",'w+') as file:
-        file.write(str((response.text).encode("utf-8")))
+    with open("main.py",'w+',encoding='utf-8') as file:
+        file.write(response.text)
     file.close()
+
+    # Windows to Mac / Linux / Unix Encoding
+    if os.name == "nt":
+        with open("main.py",'rb') as file:
+            content = file.read()
+        content.replace(b'\r\n', b'\n')
+        with open("main.py",'wb') as file:
+            file.write(content)
+        file.close()
+    
+    # Mac / Linux / Unix to Windows Encoding
+    if os.name == "posix":
+        with open("main.py",'rb') as file:
+            content = file.read()
+        content.replace(b'\n', b'\r\n')
+        with open("main.py",'wb') as file:
+            file.write(content)
+        file.close()
+    
+    # Unsupported Operating System
+    else:
+        print(f'Your operating system "{os.name}" is not supported.')
+        exit()
