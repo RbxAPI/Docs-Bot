a7e4df8982477dd6253ef71b41b34f53c4d96069
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-06-04 13:38:58 -0400

    Implement maintenance commands

---
+++ cogs/maintenance.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+
+class Maintenance(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    # Maintenance Commands
+    @commands.command(name='reload')
+    async def reload(self, ctx, *, name: str):
+        try:
+            self.bot.unload_extension(name)
+            self.bot.load_extension(name)
+        except Exception as error:
+            await ctx.send(f'Error: {error}')
+        else:
+            await ctx.send(f'Reloaded: {name}')
+
+def setup(bot):
+    bot.add_cog(Maintenance(bot))
