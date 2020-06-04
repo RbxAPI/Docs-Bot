802bcb3a208b89d5436e187a8835dad1e0a61854
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-06-04 15:28:20 -0400

    Integrate / implement database solution

--- cogs/data.py
+++ cogs/data.py
@@ -1,1 +1,61 @@
 import sqlite3
+
+connector = sqlite3.connect('storage.db')
+
+class Data:
+
+    def __init__(self):
+        self.connector = sqlite3.connect('storage.db')
+        self.modEntry = self.modEntry(self)
+
+        # Setup Database if isn't valid
+        try: 
+            connector.execute('''CREATE TABLE modLogs (
+                ACTION TEXT NOT NULL,
+                DURATION INTEGER NULL,
+                REASON TEXT NOT NULL,
+                MODERATOR INTEGER NOT NULL,
+                TARGET INTEGER NOT NULL,
+                DATE TEXT NOT NULL
+            );''')
+        except Exception as error:
+            print(f'db init Exception: {error}')
+            pass # Database table is already created
+    
+    class modEntry:
+
+        def __init__(self, Data):
+            self.Data = Data
+            self.connector = Data.connector
+            self.cursor = self.connector.cursor()
+        
+        def insert(self, action, duration, reason, moderator, target, date):
+            try:
+                self.cursor.execute('''INSERT INTO modLogs (ACTION, DURATION, REASON, MODERATOR, TARGET, DATE) VALUES (?, ?, ?, ?, ?, ?);''', (action, duration, reason, moderator, target, date))
+                self.connector.commit()
+                self.cursor.close()
+            except sqlite3.Error as error:
+                print(f'{error}')
+        
+        def fetch(self, id):
+            result = []
+            index = 0
+            try:
+                self.cursor.execute('''SELECT * FROM modLogs''')
+            except sqlite3.Error as error:
+                print(f'{error}')
+            
+            for action, duration, reason, moderator, target, date in self.cursor.fetchall():
+                index += 1
+                print(index, action, duration, reason, moderator, target, date)
+                if int(target) == id:
+                    result.append({
+                        "index": index,
+                        "action": action,
+                        "duration": duration,
+                        "reason": reason,
+                        "moderator": moderator,
+                        "target": target,
+                        "date": date
+                    })
+            return result
--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 from discord import utils
+from .data import Data
+
+db = Data() # Initialize database
 
 class Moderation(commands.Cog):
 
@@ -28,9 +31,6 @@ class Moderation(commands.Cog):
         log_channel = self.bot.get_channel(709262930750865440) # moderation-logs channel
 
         # Create default embed
-        entries = await ctx.message.guild.audit_logs(limit=None, action=discord.AuditLogAction.channel_update).flatten()
-        base_entry = entries[0]
-
         emb = discord.Embed()
         emb.set_author(name="Moderation", icon_url="https://cdn.discordapp.com/attachments/336577284322623499/683028692133216300/ac6e275e1f638f4e19af408d8440e1d1.png")
         emb.set_footer(text=f'\t\t\t\t\t\t\tTimestamp: {ctx.message.created_at}')
@@ -38,14 +38,14 @@ class Moderation(commands.Cog):
         # If Moderator invokes, mute from server and not just channel
         if ((utils.get(ctx.guild.roles, name="Moderator") in ctx.message.author.roles)):
             emb.add_field(name="Mute (Server)", value=f'{member.mention} ({member.id})', inline=False)
-            emb.add_field(name="Duration", value=f'n/a', inline=False)
-            emb.add_field(name="Reason", value=f'n/a', inline=False)
+            emb.add_field(name="Duration", value=f'{duration}', inline=False)
+            emb.add_field(name="Reason", value=f'{message}', inline=False)
             emb.add_field(name="Moderator", value=f'{ctx.message.author}#{ctx.message.author.discriminator} ({ctx.message.author.id})', inline=False)
             await member.add_roles(role)
 
             # Add data to database
-            # (Action, Duration, Reason, Moderator)
-
+            # (Action, Duration, Reason, Moderator, Target, Date)
+            db.modEntry.insert("Mute (Server)", duration, message, ctx.message.author.id, member.id, ctx.message.created_at)
 
         # If Library Developer invokes, mute from channel and not server
         elif ((utils.get(ctx.guild.roles, name="Library Developer") in ctx.message.author.roles) and (channel.category.name == "libraries" or channel.category.name == "frameworks")):
@@ -56,7 +56,8 @@ class Moderation(commands.Cog):
             await channel.set_permissions(member, read_messages=True, send_messages=False)
 
             # Add data to database
-            # (Action, Duration, Reason, Moderator)
+            # (Action, Duration, Reason, Moderator, Target, Date)
+            db.modEntry.insert("Mute (Channel)", duration, message, ctx.message.author.id, member.id, ctx.message.created_at)
         
         await ctx.message.add_reaction('👍')
         await log_channel.send(embed=emb)
@@ -69,9 +70,6 @@ class Moderation(commands.Cog):
         log_channel = self.bot.get_channel(709262930750865440) # moderation-logs channel
 
         # Create default embed
-        entries = await ctx.message.guild.audit_logs(limit=None, action=discord.AuditLogAction.channel_update).flatten()
-        base_entry = entries[0]
-
         emb = discord.Embed()
         emb.set_author(name="Moderation", icon_url="https://cdn.discordapp.com/attachments/336577284322623499/683028692133216300/ac6e275e1f638f4e19af408d8440e1d1.png")
         emb.set_footer(text=f'\t\t\t\t\t\t\tTimestamp: {ctx.message.created_at}')
@@ -79,26 +77,68 @@ class Moderation(commands.Cog):
         # If Moderator invokes, unmute from server and not just channel
         if ((utils.get(ctx.guild.roles, name="Moderator") in ctx.message.author.roles)):
             emb.add_field(name="Unmute (Server)", value=f'{member.mention} ({member.id})', inline=False)
-            emb.add_field(name="Duration", value=f'n/a', inline=False)
-            emb.add_field(name="Reason", value=f'n/a', inline=False)
+            emb.add_field(name="Reason", value=f'{message}', inline=False)
             emb.add_field(name="Moderator", value=f'{ctx.message.author}#{ctx.message.author.discriminator} ({ctx.message.author.id})', inline=False)
             await member.remove_roles(role)
 
             # Add data to database
-            # (Action, Duration, Reason, Moderator)
+            # (Action, Duration, Reason, Moderator, Target, Date)
+            db.modEntry.insert("Unmute (Server)", None, message, ctx.message.author.id, member.id, ctx.message.created_at)
 
         # If Library Developer invokes, unmute from channel and not server
         elif ((utils.get(ctx.guild.roles, name="Library Developer") in ctx.message.author.roles) and (channel.category.name == "libraries" or channel.category.name == "frameworks")):
             emb.add_field(name="Unmute (Channel)", value=f'{member.mention} ({member.id})', inline=False)
-            emb.add_field(name="Reason", value=f'n/a', inline=False)
+            emb.add_field(name="Reason", value=f'{message}', inline=False)
             emb.add_field(name="Moderator", value=f'{ctx.message.author}#{ctx.message.author.discriminator} ({ctx.message.author.id})', inline=False)
             await channel.set_permissions(member, overwrite=None)
 
-            # Add data to json
-            # (Action, Duration, Reason, Moderator)
+            # Add data to database
+            # (Action, Duration, Reason, Moderator, Target, Date)
+            db.modEntry.insert("Unmute (Channel)", None, message, ctx.message.author.id, member.id, ctx.message.created_at)
         
         await ctx.message.add_reaction('👍')
         await log_channel.send(embed=emb)
+
+    @commands.command()
+    @commands.has_role("Library Developer" or "Moderator")
+    async def warn(self, ctx, member: discord.Member, *, message):
+        channel = ctx.message.channel
+        log_channel = self.bot.get_channel(709262930750865440) # moderation-logs channel
+
+        # Create default embed
+        emb = discord.Embed()
+        emb.set_author(name="Moderation", icon_url="https://cdn.discordapp.com/attachments/336577284322623499/683028692133216300/ac6e275e1f638f4e19af408d8440e1d1.png")
+        emb.set_footer(text=f'\t\t\t\t\t\t\tTimestamp: {ctx.message.created_at}')
+        emb.add_field(name="Warn", value=f'{member.mention} ({member.id})', inline=False)
+        emb.add_field(name="Reason", value=f'{message}', inline=False)
+        emb.add_field(name="Moderator", value=f'{ctx.message.author}#{ctx.message.author.discriminator} ({ctx.message.author.id})', inline=False)
+            
+        # Add data to database
+        # (Action, Duration, Reason, Moderator, Target, Date)
+        db.modEntry.insert("Warn", None, message, ctx.message.author.id, member.id, ctx.message.created_at)
+            
+        await ctx.message.add_reaction('👍')
+        await log_channel.send(embed=emb)
+    
+    @commands.command()
+    @commands.has_role("Moderator")
+    async def infractions(self, ctx, member: discord.Member):
+        i = 0
+        
+        # Fetch data from database
+        query = db.modEntry.fetch(member.id)
+        emb = discord.Embed()
+        emb.set_author(name="Moderation", icon_url="https://cdn.discordapp.com/attachments/336577284322623499/683028692133216300/ac6e275e1f638f4e19af408d8440e1d1.png")
+        emb.set_footer(text=f'\t\t\t\t\t\t\tTimestamp: {ctx.message.created_at}')
+
+        for case in query:
+            emb.add_field(name=f'Case Number: {case["index"]}', value=f'Action: {case["action"]}\nDuration: {case["duration"]}\nUser: {case["target"]}\nReason: {case["reason"]}\nModerator: {case["moderator"]}\n Date: {case["date"]}', inline=False)
+            if i >= 25:
+                await ctx.send(embed=emb)
+                i = 0
+            i += 1
+        await ctx.send(embed=emb)
+        
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
