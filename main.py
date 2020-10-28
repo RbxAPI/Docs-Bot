7e91d5f2cc20b69c5d78c50dbb8d24a44925e802
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-10-27 23:29:03 -0400

    [Errors] Better error handling.

--- main.py
+++ main.py
@@ -27,18 +27,19 @@ async def on_error(event, *args, **kwargs):
     channel = channel = bot.get_channel(770843267327721502)
     emb = discord.Embed(color=discord.Color.red())
     emb.set_author(name="Error (Event)", icon_url="https://cdn.discordapp.com/attachments/336577284322623499/683028692133216300/ac6e275e1f638f4e19af408d8440e1d1.png")
-    emb.add_field(name="Event", value=event)
-    emb.add_field(name="Traceback", value=traceback.format_exc())
+    emb.add_field(name="Event", value=event, inline=False)
+    emb.add_field(name="Traceback", value=traceback.format_exc(), inline=False)
     await channel.send(embed=emb)
 
 # Command Error
 @bot.event
-async def on_command_error(message, error):
+async def on_command_error(ctx, error):
     channel = channel = bot.get_channel(770843267327721502)
     emb = discord.Embed(color=discord.Color.red())
     emb.set_author(name="Error (Commands)", icon_url="https://cdn.discordapp.com/attachments/336577284322623499/683028692133216300/ac6e275e1f638f4e19af408d8440e1d1.png")
-    emb.add_field(name="Error", value=error)
-    emb.add_field(name="Message", value=message)
+    emb.add_field(name="Error", value=error, inline=False)
+    emb.add_field(name="Command", value=ctx.command, inline=False)
+    emb.add_field(name="Message", value=ctx.message, inline=False)
     await channel.send(embed=emb)
 
 if __name__ == '__main__':
