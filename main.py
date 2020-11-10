16727f4e4df241ae55faf878f6989e9d1ee30697
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-11-10 01:31:50 -0500

    [Verification] Disabled Module.

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # A list containing all cogs that we want to load
-available_cogs = ['cogs.maintenance', 'cogs.logging', 'cogs.utility', 'cogs.channels', 'cogs.moderation',
-                  'cogs.verification', 'cogs.tagging']
+available_cogs = ['cogs.maintenance', 'cogs.logging', 'cogs.utility', 'cogs.channels', 'cogs.moderation', 'cogs.tagging']
+
+# Disabled Cogs
+# 'cogs.verification'
 
 # Bot Init
 description = "Roblox API Server Documentation Bot"
